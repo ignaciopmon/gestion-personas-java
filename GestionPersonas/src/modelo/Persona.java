35043d8ca8ea8fe18b2c0aa2e726fa1cf83f84bb
ignaciopmon <ignaciopmonroy@gmail.com>
2025-11-23 13:59:28 +0100

    Añadido atributo curso

--- GestionPersonas/src/modelo/Persona.java
+++ GestionPersonas/src/modelo/Persona.java
@@ -2,14 +2,18 @@ package modelo;
 public class Persona {
     private String nombre;
     private int edad;
+    private String curso;
 
-    public Persona(String nombre, int edad) {
+    // 2. Constructor actualizado
+    public Persona(String nombre, int edad, String curso) {
         this.nombre = nombre;
         this.edad = edad;
+        this.curso = curso;
     }
 
     public void mostrarDatos() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
+        System.out.println("Curso: " + curso);
     }
 }
